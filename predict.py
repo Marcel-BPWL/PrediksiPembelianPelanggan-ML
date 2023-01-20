6d1eee82cd3317a2775467245c699be7a9721083
MarcelOwen <84946802+Marcel-BPWL@users.noreply.github.com>
2023-01-20 21:37:14 +0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -3,21 +3,21 @@ import pandas as pd
 import pickle
 def app():
     option_basket = st.sidebar.selectbox(
-     'Did the customer add any item to the basket?',
+     'Apakah pelanggan menambahkan item kedalam keranjang?',
     ('Yes', 'No'))
     option_promo = st.sidebar.selectbox(
-     'Did the customer click on any Promo banner?',
+     'Apakah pelanggan mengklik banner Promo?',
     ('Yes', 'No'))
     option_sign_in = st.sidebar.selectbox(
-     'Did the customer sign in?',
+     'Apakah pelanggan melakukan Login?',
     ('Yes', 'No'))
     option_home = st.sidebar.selectbox(
-     'Did the customer see the home page?',
+     'Apakah pelanggan melihat halaman utama?',
     ('Yes', 'No'))
     option_repeat = st.sidebar.selectbox(
-     'Is he a repeating customer?',
+     'Apakah dia pelanggan tetap?',
      ('Yes', 'No'))
-    if st.sidebar.button('Predict the Possibility of the Customer to make a purchase'):
+    if st.sidebar.button('Memprediksi Kemungkinan Pelanggan melakukan pembelian'):
         lookup_dict={"Yes":1,"No":0}
         dict = {'basket_add_detail':[lookup_dict[option_basket]],
             'promo_banner_click':[lookup_dict[option_promo]],
@@ -26,15 +26,15 @@ def app():
             "returning_user":[lookup_dict[option_repeat]]
            }
         prediction_df = pd.DataFrame(dict)
-        st.write("Customer details for Propensity prediction")
+        st.write("Detail pelanggan untuk prediksi keinginan melakukan pembelian")
         st.write(prediction_df)
         with open("propensity_model.pkl", 'rb') as pfile:  
             propensity_model_loaded=pickle.load(pfile)
         y_predicted=propensity_model_loaded.predict(prediction_df)
         if (y_predicted[0]==1):
-            st.write("The customer will order from the website. Probabality of ordering:")
+            st.write("Pelanggan akan melakukan pemesanan dari situs web. Probabilitas pemesanan:")
             # st.write(y_predicted[0])
         else:
-            st.write("The customer will not order from the website. Probabality of ordering:")
+            st.write("Pelanggan tidak akan melakukan pemesanan dari situs web. Probabilitas pemesanan:")
             # st.write(y_predicted[0])
         st.write(propensity_model_loaded.predict_proba(prediction_df))
